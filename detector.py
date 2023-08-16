8efa115d57f2fe31eba23fbc41c400be1d1f5eaa
keatox <keatx05@gmail.com>
2023-08-16 15:54:14 -0500

    Update detector.py

--- detector.py
+++ detector.py
@@ -10,14 +10,14 @@ while True:
     _, image = webcam.read()
     #converts image into greyscale and runs detection
     greyimage = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    face = haar_cascade.detectMultiScale(greyimage, 1.3, 4)
+    face = haar_cascade.detectMultiScale(greyimage, 1.3, 3)
 
     #display detected face inside rectangle
     for (x,y,w,h) in face:
         cv2.rectangle(image,(x,y),(x+w,y+h),(200,200,0),2)
 
     #opens display window
-    cv2.imshow("Eye Detector",image)
+    cv2.imshow("Face Detector",image)
 
     #stops program after pressing esc
     key = cv2.waitKey(10)
