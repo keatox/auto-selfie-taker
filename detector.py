d6e0c392bf857be70230839320853f83a22b57b0
keatox <keatx05@gmail.com>
2023-08-26 15:34:37 -0500

    Update detector.py

--- detector.py
+++ detector.py
@@ -16,25 +16,17 @@ while True:
     #converts image into greyscale and runs detection
     greyimage = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     face = face_cascade.detectMultiScale(greyimage, 1.3, 3)
-
-    #display detected face inside rectangle
     for (x,y,w,h) in face:
-        cv2.rectangle(image,(x,y),(x+w,y+h),(200,200,0),2)
-
-        #display smile in rectangle when detected
         bound_gray = greyimage[y:y+h, x:x+w]
         bound_color = image[y:y+h, x:x+w]
-        smile = smile_cascade.detectMultiScale(bound_gray,1.8,40) 
-        for (i, j, a, b) in smile:
-            cv2.rectangle(bound_color,(i,j), (i+a,j+b),(0,255,0),2)
-
+        smile = smile_cascade.detectMultiScale(bound_gray,1.8,20) 
         #takes selfie if smile is detected and saves to local folder
         if not smile_detected and len(smile)>0:
             smile_detected = True
             start_time = time.time()
         elif not len(smile)>0:
             smile_detected = False
-        elif smile_detected and time.time() - start_time >= 0.5:
+        elif smile_detected and time.time() - start_time >= 1:
             filename = "selfie{}.jpg".format(counter)
             cv2.imwrite(filename,image)
             print("Photo Captured")
